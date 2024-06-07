a642b97d089c8b5825a9837345e5203a126db119
jairo ramos <jaramosm@ufpso.edu.co>
2024-06-07 17:53:24 -0500

    a

---
+++ src/components/vue/LoadData.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+}
+
+</script>
+
+<template>
+    <div class="bg-slate-300 mb-1">
+        sjdl afaoldñ
+    </div>
+    <button @click="loadData">
+        Cargar info
+    </button>
+</template>
